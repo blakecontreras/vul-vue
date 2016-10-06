0fc4b88d782135acdf5c9290ee85ad4a21a3da87
Blake Contreras <shardsofsilver@gmail.com>
2016-10-06 16:52:33 -0700

    Add Phrase component

---
+++ src/phrase/Phrase.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+<div class="jumbotron col-md-8">
+  <div>
+    <h2>You {{ adjective.text }} {{ noun.text }}!</h2>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['adjective', 'noun']
+};
+</script>
+
+<style lang="css" scoped>
+.jumbotron { margin-top: 30px; }
+</style>
