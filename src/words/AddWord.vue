80ff02fed99786ecfcae0ef034f2b62771e9a747
Blake Contreras <shardsofsilver@gmail.com>
2016-10-06 17:54:20 -0700

    Add Add Word component

---
+++ src/words/AddWord.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+<div class="jumbotron">
+  <form v-on:submit="handleSubmit()" v-if="active">
+  <h4>Add Words Here!</h4>
+    <div class="form-group">
+      <input type="text" class="form-control" name="adjective" placeholder="Adjective"
+      v-model="newAdjective.text">
+    </div>
+
+    <div class="form-group">
+      <input type="text" class="form-control" name="noun" placeholder="Noun"
+      v-model="newNoun.text">
+    </div>
+
+    <button type="submit" class="btn btn-lg btn-block btn-primary">
+      Add Word(s)
+    </button>
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: true,
+      newAdjective: { id: null, text: null, selected: false },
+      newNoun: { id: null, text: null, selected: false }
+    };
+  },
+  methods: {
+    handleSubmit() {
+      $emit('wordAdded', { adjective: newAdjective, noun: newNound });
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+form {
+  padding: 5px;
+  background: #ECF0F1;
+  border-radius: 2px;
+}
+</style>
